3e4c919e0d232bb566fb02b16c0d2abe36480368
jdr4343 <jdr4343@naver.com>
2021-06-18 21:12:44 +0900

    Initial Commit

---
+++ FirstToyProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstToyProject
+//
+//  Created by 신지훈 on 2021/06/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
