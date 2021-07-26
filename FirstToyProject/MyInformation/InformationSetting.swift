fe4222dbdb2ef426dfd3f3c7ea9004cf07e1387f
jdr4343 <jdr4343@naver.com>
2021-07-26 21:00:05 +0900

    information

--- FirstToyProject/MyInformation/InformationSetting.swift
+++ FirstToyProject/MyInformation/InformationSetting.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+enum CellType: String {
+    case action
+    case disclosure
+    case `switch`
+    case checkmark
+}
+
+class PhotoSettingItem {
+    init(type: CellType, title: String, on: Bool = false, imageName: String? = nil) {
+        self.type = type
+        self.title = title
+        self.on = on
+    self.imageName = imageName
+    }
+
+    let type: CellType
+    let title: String
+    var on: Bool
+    var imageName: String?
+    }
+
+class PhotoSettingSection {
+    init(items: [PhotoSettingItem], header: String? = nil, footer: String? = nil) {
+        self.items = items
+        self.header = header
+        self.footer = footer
+    }
+    let items: [PhotoSettingItem]
+    var header: String?
+    var footer: String?
+}
